43a26be55556550ed26cca7125fe35967b25dc33
Ben <Benjamin.Good.nth@gmail.com>
2026-01-21 11:43:15 -0500

    Adding keys and updating the x and y coordinates to better render on the screen

--- h2o_debug_menu/menu.h
+++ h2o_debug_menu/menu.h
@@ -7,101 +7,123 @@
 void debug_draw_add_menu(Ili9488Defines screen);
 void debug_draw_main_menu(Ili9488Defines screen);
 
+enum AddMenuKeys {
+    ADD_BUTTON_ONE,
+    ADD_BUTTON_TWO,
+    ADD_BUTTON_THREE,
+    ADD_BUTTON_FOUR,
+    ADD_BUTTON_FIVE,
+    ADD_BUTTON_SIX,
+    ADD_BUTTON_SEVEN,
+    ADD_BUTTON_EIGHT,
+    ADD_BUTTON_NINE,
+    ADD_BUTTON_ZERO,
+    ADD_BUTTON_DELETE,
+    ADD_BUTTON_ADD_SN,
+};
+
+enum MainMenuKeys {
+    MAIN_BUTTON_EXIT,
+    MAIN_BUTTON_CLEAR_SN_LIST,
+    MAIN_BUTTON_ADD_SN,
+};
+
 static const TouchBox add_touch_boxes[] = {
     /* #1 */
     {
-        .xs = 4,
-        .ys = 24,
-        .xe = 105,
-        .ye = 125,
+        .key = ADD_BUTTON_ONE,
+        .xs = 9,
+        .ys = 69,
+        .xe = 110,
+        .ye = 170,
     },
-
     /* 2 */
     {
-        .xs = 106,
-        .ys = 24,
-        .xe = 207,
-        .ye = 125,
+        .key = ADD_BUTTON_TWO,
+        .xs = 111,
+        .ys = 69,
+        .xe = 212,
+        .ye = 170,
     },
-
     /* 3 */
     {
-        .xs = 208,
-        .ys = 24,
-        .xe = 309,
-        .ye = 125,
+        .key = ADD_BUTTON_THREE,
+        .xs = 213,
+        .ys = 69,
+        .xe = 314,
+        .ye = 170,
     },
-
     /* #4 */
     {
-        .xs = 4,
-        .ys = 126,
-        .xe = 105,
-        .ye = 227,
+        .key = ADD_BUTTON_FOUR,
+        .xs = 9,
+        .ys = 171,
+        .xe = 110,
+        .ye = 272,
     },
-
     /* 5 */
     {
-        .xs = 106,
-        .ys = 126,
-        .xe = 207,
-        .ye = 227,
+        .key = ADD_BUTTON_FIVE,
+        .xs = 111,
+        .ys = 171,
+        .xe = 212,
+        .ye = 272,
     },
-
     /* 6 */
     {
-        .xs = 208,
-        .ys = 126,
-        .xe = 309,
-        .ye = 227,
+        .key = ADD_BUTTON_SIX,
+        .xs = 213,
+        .ys = 171,
+        .xe = 314,
+        .ye = 272,
     },
-
     /* #7 */
     {
-        .xs = 4,
-        .ys = 228,
-        .xe = 105,
-        .ye = 329,
+        .key = ADD_BUTTON_SEVEN,
+        .xs = 9,
+        .ys = 273,
+        .xe = 110,
+        .ye = 374,
     },
-
     /* 8 */
     {
-        .xs = 106,
-        .ys = 228,
-        .xe = 207,
-        .ye = 329,
+        .key = ADD_BUTTON_EIGHT,
+        .xs = 111,
+        .ys = 273,
+        .xe = 212,
+        .ye = 374,
     },
-
     /* 9 */
     {
-        .xs = 208,
-        .ys = 228,
-        .xe = 309,
-        .ye = 329,
+        .key = ADD_BUTTON_NINE,
+        .xs = 213,
+        .ys = 273,
+        .xe = 314,
+        .ye = 374,
     },
-
     /* Delete */
     {
-        .xs = 4,
-        .ys = 330,
-        .xe = 105,
-        .ye = 431,
+        .key = ADD_BUTTON_DELETE,
+        .xs = 9,
+        .ys = 375,
+        .xe = 110,
+        .ye = 476,
     },
-
     /* 0 */
     {
-        .xs = 106,
-        .ys = 330,
-        .xe = 207,
-        .ye = 431,
+        .key = ADD_BUTTON_ZERO,
+        .xs = 111,
+        .ys = 375,
+        .xe = 212,
+        .ye = 476,
     },
-
     /* Add */
     {
-        .xs = 208,
-        .ys = 330,
-        .xe = 309,
-        .ye = 431,
+        .key = ADD_BUTTON_ADD_SN,
+        .xs = 213,
+        .ys = 375,
+        .xe = 314,
+        .ye = 476,
     },
 };
 
